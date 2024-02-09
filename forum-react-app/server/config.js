815a068efbdb5097ef271dc0d167972a1d0ec79c
Joshua <119659329+elib00@users.noreply.github.com>
2024-02-10 00:42:26 +0800

    updated config.js

--- forum-react-app/server/config.js
+++ forum-react-app/server/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongodbURL = `mongodb+srv://joshuanapinas1019:AppleButter1019@mern-forum.10gvjfy.mongodb.net/?retryWrites=true&w=majority`
+export const mongodbURL = `mongodb+srv://<username>:<password>@mern-forum.10gvjfy.mongodb.net/?retryWrites=true&w=majority`
